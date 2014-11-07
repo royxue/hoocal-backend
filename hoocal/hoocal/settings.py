ca6f9aaba5aa37ca7437668bf6ff25fc3ce9aa55
Roy Xue <xljroy@gmail.com>
2014-11-08 00:40:44 +0800

    Modify settings

--- hoocal/hoocal/settings.py
+++ hoocal/hoocal/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hocalen'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,8 @@ WSGI_APPLICATION = 'hoocal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hoocal',
     }
 }
 
