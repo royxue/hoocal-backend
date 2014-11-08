49793ba3d2364e79cce246598d8b119f5d77dba3
Roy Xue <xljroy@gmail.com>
2014-11-08 14:28:31 +0800

    Add admin

--- hoocal/hocalen/admin.py
+++ hoocal/hocalen/admin.py
@@ -5,9 +5,26 @@ from hocalen.models import User, Event, Org, Comment
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'nickname', 'email')
-    fields = ('nickname', 'email', 'password', 'avatar')
+    fields = ('nickname', 'email', 'password', 'avatar', 'subscribe_events')
 
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_by', 'created_at', 'org')
-    fields = ()
+    fields = ('title', 'content', 'created_by', 'created_at', 'last_modified',
+            'start_time', 'end_time', 'tag', 'org', 'is_public', 'is_deleted', 'like_users')
+
+
+class OrgAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'content', 'owner')
+    fields = ('name', 'content', 'owner', 'members', 'followers')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'event', 'content', 'user', 'created_at', 'reply_to')
+    fields = ('id', 'event', 'content', 'user', 'created_at', 'reply_to')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Org, OrgAdmin)
+admin.site.register(Comment, CommentAdmin)
