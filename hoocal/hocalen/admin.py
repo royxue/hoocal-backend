8869fc78a9758efea0022a3d87a747100df3823c
Roy Xue <xljroy@gmail.com>
2014-11-09 17:35:42 +0800

    Remove m2m in admin

--- hoocal/hocalen/admin.py
+++ hoocal/hocalen/admin.py
@@ -5,23 +5,22 @@ from hocalen.models import User, Event, Org, Comment
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'nickname', 'email')
-    fields = ('nickname', 'email', 'password', 'avatar', 'subscribe_events')
-
+    fields = ('nickname', 'email', 'password', 'avatar')
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_by', 'created_at', 'org')
+    list_display = ('id', 'title', 'created_by', 'created_at', 'org', 'is_public', 'is_deleted')
     fields = ('title', 'content', 'created_by', 'created_at', 'last_modified',
-            'start_time', 'end_time', 'tag', 'org', 'is_public', 'is_deleted', 'like_users')
+            'start_time', 'end_time', 'tag', 'org', 'is_public', 'is_deleted')
 
 
 class OrgAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'content', 'owner')
-    fields = ('name', 'content', 'owner', 'members', 'followers')
+    fields = ('name', 'content', 'owner', 'members')
 
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'event', 'content', 'user', 'created_at', 'reply_to')
-    fields = ('id', 'event', 'content', 'user', 'created_at', 'reply_to')
+    fields = ('id', 'event', 'content', 'user', 'created_at')
 
 
 admin.site.register(User, UserAdmin)
