3d0be98ff7e03380ae2a78bef1c2c2412d111edf
Roy Xue <xljroy@gmail.com>
2014-11-09 17:55:36 +0800

    Modify Models

--- hoocal/hocalen/models.py
+++ hoocal/hocalen/models.py
@@ -3,15 +3,15 @@ from django.utils.timezone import now
 
 
 class Event(models.Model):
-    title = models.TextField()
+    title = models.CharField(maxlength=100)
     content = models.TextField()
     created_by = models.ForeignKey('User')
     created_at = models.DateTimeField(default=now)
     last_modified = models.DateTimeField(default=now)
-    start_time = models.DateTimeField()
+    start_time = models.DateTimeField(default=now)
     end_time = models.DateTimeField()
-    tag = models.TextField()
-    org = models.ForeignKey('Org', related_name='events')
+    tag = models.TextField(blank=True)
+    org = models.ForeignKey('Org', related_name='events', null=True,)
     is_public = models.BooleanField(default=True)
     is_deleted = models.BooleanField(default=False)
     like_users = models.ManyToManyField('User', related_name='like_events', db_table='hoocal_event_like', null=True)
@@ -19,8 +19,9 @@ class Event(models.Model):
     def __unicode__(self):
         return unicode(self.title)
 
+
 class Org(models.Model):
-    name = models.TextField()
+    name = models.CharField(maxlength=100)
     content = models.TextField()
     owner = models.ForeignKey('User', related_name='own_orgs')
     members = models.ManyToManyField('User', related_name='member_orgs', db_table='hoocal_org_member')
@@ -29,10 +30,11 @@ class Org(models.Model):
     def __unicode__(self):
         return unicode(self.name)
 
+
 class User(models.Model):
-    nickname = models.TextField()
+    nickname = models.CharField(maxlength=100)
     email = models.EmailField()
-    password = models.TextField()
+    password = models.CharField(maxlength=50)
     avatar = models.URLField()
     subscribe_events = models.ManyToManyField('Event', related_name='subscribe_user', db_table='hoocal_subscribe', null=True)
     
@@ -53,11 +55,4 @@ class UserToken(models.Model):
     token = models.CharField(max_length=64)
     expired_at = models.DateTimeField()
     is_expired = models.BooleanField(default=False)
-
-
-
-    
-
-
-
 
